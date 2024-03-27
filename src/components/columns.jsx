312e2d7afa31e3dbd5d28e7faafc28d1931b3393
Aleksandr.Mukhorkin@oag.com <Aleksandr.Mukhorkin@oag.com>
2024-03-27 12:15:50 +0200

    Initial commit

---
+++ src/components/columns.jsx
@@ -0,0 +1,34 @@
+export const COLUMNS = [
+    {
+        accessor: "FlightDesignator",
+        Header: "Flight Designator",
+    },
+    {
+        accessor: "DepartureDate",
+        Header: "Departure Date",
+    },
+    {
+        accessor: "SequenceNumber",
+        Header: "Sequence #",
+    },
+    {
+        accessor: "Stops",
+        Header: "Stops",
+    },
+    {
+        accessor: "DepartureAirport",
+        Header: "Departure Airport",
+    },
+    {
+        accessor: "DepartureTime",
+        Header: "Departure Time",
+    },
+    {
+        accessor: "ArrivalAirport",
+        Header: "Arrival Airport",
+    },
+    {
+        accessor: "ArrivalTime",
+        Header: "Arrival Time",
+    }, 
+]
